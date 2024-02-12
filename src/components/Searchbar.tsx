a8c76a546674d0a4cc7f52c28401392d877b3643
AdrianLicko <licko.adrian03@gmail.com>
2024-02-12 18:08:18 +0100

    changed a bit of style

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,8 +1,8 @@
-
+import './Searchbar.css'
 
 const Searchbar = ({ setSearchText }: { setSearchText: (text: string) => void }) => {
 
-    return <section>
+    return <section className="searchbar-container">
         <input type="text" placeholder="Search..." onChange={(e) => setSearchText(e.target.value)} />
     </section>
 };
